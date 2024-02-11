636e03ebbc6e400400adbbe2ad0c8f428a0dcadf
michalmlk <michal.melka1@gmail.com>
2024-02-11 20:17:40 +0100

    setup colors + prettier config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ["Righteous", "sans-serif"],
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        fontFamily: {
+            sans: ['Righteous', 'sans-serif'],
+            sans2: ['Barlow', 'sans-serif'],
+        },
+        extend: {
+            backgroundColor: {
+                primary2: '#FFEE32',
+                secondary: '#673DDB',
+                secondary2: '#66529C',
+                desert: '#868252',
+                background2: '#33322C',
+                card: '#4D485C',
+            },
+        },
     },
-    extend: {
-      backgroundColor: {
-        primary2: "#33332C",
-      },
-    },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
