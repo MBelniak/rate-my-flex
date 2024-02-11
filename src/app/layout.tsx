71b25e00cbb8158675eda9eed25f57e6cf83c037
MBelniak <belniakm@wp.pl>
2024-02-11 19:27:35 +0100

    BG color and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.className} bg-primary2`}>{children}</body>
       </html>
     </ClerkProvider>
   );
